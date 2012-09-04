5b91b584d023f726d26f47a94b863819a1340460
unknown <AKrieger@W7-93-DEV10.kore.korewireless.com>
2012-09-04 16:53:08 -0500

    Began work on parser

---
+++ SQLAssist/SQLParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLAssist
+{
+    class SQLParser
+    {
+
+        /*
+         * Going to pass in some manner of array or list of the fields and their values. Might have to do a custom object with table name attached
+         * Return will be INSERT INTO <tablename> (field1, field2, field3, field4, etc) VALUES('strvalue1','strvalue2',numvalue,NULL, etc)
+         
+         */
+        string ConvertToInsert()
+        {
+
+            return null;
+        }
+    }
+}
