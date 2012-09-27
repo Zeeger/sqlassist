ed4ed5d84b6c5a4d0dd0a98d25dc7fca991e82ca
Zeeger <akrieger1986@gmail.com>
2012-09-27 09:16:31 -0500

    pushing changes

---
+++ SQLAssist/Classes/SQLDataTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLAssist.Classes
+{
+	class SQLDataTable
+	{
+		public IList<SQLDataCell> _sqlDataCells;
+
+
+	}
+}
---
+++ SQLAssist/Classes/SQLDataTableTranslator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLAssist.Classes
+{
+	class SQLDataTableTranslator
+	{
+
+		public SQLDataTable SetDataTable(string input)
+		{ 
+		/*
+		 * 
+		 * Instantiate a module-level sql parser and use it to write table definitions to the return.
+		 * 
+		 * Make another class for concatenating SQLDataTables into statements
+		 * */
+		}
+	}
+}
